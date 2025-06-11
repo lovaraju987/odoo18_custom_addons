3db3c7eeacc1205a2f6c2188bc36edb26811ca70
lovaraju.mylapalli <mailapallilovaraaju987@gmail.com>
2025-06-11 12:24:56 +0530

    removed extra added latitiude and longitude fields.

--- employee_self_service_portal/models/attendance.py
+++ employee_self_service_portal/models/attendance.py
@@ -3,9 +3,5 @@ from odoo import models, fields
 class HrAttendance(models.Model):
     _inherit = 'hr.attendance'
 
-    in_latitude = fields.Char("Check-In Latitude")
-    in_longitude = fields.Char("Check-In Longitude")
-    out_latitude = fields.Char("Check-Out Latitude")
-    out_longitude = fields.Char("Check-Out Longitude")
     check_in_location = fields.Char("Check-In Location")
     check_out_location = fields.Char("Check-Out Location")
