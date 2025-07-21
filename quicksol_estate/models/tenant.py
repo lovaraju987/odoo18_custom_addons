a62cab44cc8530a65a698ca88597d8e747ef3cd7
lovaraju.mylapalli <mailapallilovaraaju987@gmail.com>
2025-07-21 11:15:22 +0530

    quicksole real estate module added

---
+++ quicksol_estate/models/tenant.py
@@ -0,0 +1,13 @@
+from odoo import models, fields
+
+class Tenant(models.Model):
+    _name = 'real.estate.tenant'
+    _description = 'Tenant'
+
+    name = fields.Char(string='Tenant Name', required=True)
+    phone = fields.Char(string='Phone Number')
+    email = fields.Char(string='Email')
+    leases = fields.One2many('real.estate.lease', 'tenant_id', string='Leases')
+    image_1920 = fields.Image('Profile Picture', max_width=1920, max_height=1920)
+    occupation = fields.Char('Occupation')
+    birthdate = fields.Date('Birthdate')
